7d4970eaabf056559d4ba83594c739276e4c7406
b-rad-c <bradcorlett@gmail.com>
2022-02-08 00:08:20 -0800

    update discord link on ui and in white paper

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -1,6 +1,6 @@
 export const dBranchTwitter = 'https://twitter.com/dBranchDAO'
 export const dBranchGettr = 'https://gettr.com/user/dbranchdao'
-export const dBranchDiscord = 'https://discord.gg/k8vJE5Jf'
+export const dBranchDiscord = 'https://discord.gg/UJ7zraNA9R'
 export const dBranchWebsite = 'http://dbranch.news'
 export const dBranchEmailList = 'https://forms.gle/mgGM7CR93e3Dbvh76'
 export const dBranchVolunteerForm = 'https://forms.gle/Ui2YFUPNQ4dBhiWx7'
