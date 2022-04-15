67469ac3da81626a9fe776aeb5facee9f3269bc4
b-rad-c <bradcorlett@gmail.com>
2022-04-15 12:40:54 -0700

    begin nami integration

--- ui/src/routes/DBranchApp.js
+++ ui/src/routes/DBranchApp.js
@@ -2,6 +2,7 @@ import { create } from 'ipfs-http-client'
 import { listDir, loadArticleFromIPFS, ArticleList, ArticleReaderModal } from 'dbranch-core'
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { Spinner, Button } from 'react-bootstrap'
 
 const ipfs = create('https://ipfs.dBranch.news')
 const curatedPrefix = '/dBranch/curated'
@@ -12,6 +13,7 @@ export default function DBranchApp() {
 
     const [articleNames, setArticleNames] = useState([])
     const [articles, setArticles] = useState([])
+    const [loadingArticles, setLoadingArticles] = useState(true)
     const [selectedIndex, setSelectedIndex] = useState(-1)  // -1 means no article is selected
     const selectedArticle = selectedIndex >= 0 ? articles[selectedIndex] : null
     const [showArticle, setShowArticle] = useState(false)
@@ -29,7 +31,7 @@ export default function DBranchApp() {
                 setArticleNames(result)
             }).catch((error) => { 
                 console.error(error) 
-            })
+            }).finally(() => { setLoadingArticles(false) })
     }, []);
 
     useEffect(() => { 
@@ -61,6 +63,48 @@ export default function DBranchApp() {
     // eslint-disable-next-line
     }, [articleNames]);
 
+    //
+    // wallet tests
+    //
+
+    const [walletAPI, setWalletAPI] = useState(null)
+    const [walletNetwork, setWalletNetwork] = useState(null)
+    const [walletError, setWalletError] = useState(null)
+
+    const connectNami = () => {
+        window.cardano.nami.enable()
+            .then((api) => {
+                console.log(api)
+                setWalletAPI(api)
+            }).catch((error) => { 
+                console.error(error) 
+                if (error.code === -3) {
+                    setWalletError('Wallet connection refused')
+                }else{
+                    setWalletError('Wallet connection failed')
+                }
+                setTimeout(() => { setWalletError(null) }, 5000)
+            })
+    }
+
+    useEffect(() => {
+        if(walletAPI !== null) {
+            walletAPI.getNetworkId()
+            .then((network) => {
+                switch(network) {
+                    case 0: setWalletNetwork('cardano testnet'); break
+                    case 1: setWalletNetwork('cardano mainnet'); break
+                    default: setWalletNetwork('unknown cardano network: ' + network); break
+                }
+            }).catch((error) => {
+                console.error(error)
+                setWalletError('Error getting wallet network')
+                setTimeout(() => { setWalletError(null) }, 5000)
+            })
+        }
+    }, [walletAPI])
+
+
 return (
 <main>
     <h1>dBranch app</h1>
@@ -71,8 +115,17 @@ return (
                             show={showArticle} 
                             closeArticle={closeArticle}   
                             />}
-        <ArticleList gap={2} theme='bubble' onItemClick={openArticle} articles={articles}/>
 
+        <Button variant='primary' onClick={connectNami} disabled={walletAPI !== null}>Connect Nami</Button>
+        &emsp;
+        {walletError && <span className='text-danger'>{walletError}</span>}
+        {walletNetwork && <span>connected to: {walletNetwork}</span>}
+            
+        <hr />
+
+        {loadingArticles && <div className='text-center'><Spinner animation='border' /></div>}
+        <ArticleList gap={2} theme='bubble' onItemClick={openArticle} articles={articles}/>
+        
         <hr />
 
     </div>
