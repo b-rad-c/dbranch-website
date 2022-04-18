b3314cfc6f1bc9d50bdfc38a77548b649782dd87
b-rad-c <bradcorlett@gmail.com>
2022-04-17 17:05:52 -0700

    add PoC metadata transaction

--- ui/src/routes/DBranchApp.js
+++ ui/src/routes/DBranchApp.js
@@ -3,6 +3,7 @@ import { listDir, loadArticleFromIPFS, ArticleList, ArticleReaderModal } from 'd
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Spinner, Button } from 'react-bootstrap'
+//import * as CardanoWASM from '@emurgo/cardano-serialization-lib-browser'
 
 const ipfs = create('https://ipfs.dBranch.news')
 const curatedPrefix = '/dBranch/curated'
@@ -68,14 +69,24 @@ export default function DBranchApp() {
     //
 
     const [walletAPI, setWalletAPI] = useState(null)
+    const [walletAddress, setWalletAddress] = useState(null)
     const [walletNetwork, setWalletNetwork] = useState(null)
     const [walletError, setWalletError] = useState(null)
+    // const [cardanoWASM, setCardanoWASM] = useState(null)
 
-    const connectNami = () => {
+    async function connectNami() {
         window.cardano.nami.enable()
-            .then((api) => {
-                console.log(api)
-                setWalletAPI(api)
+            .then(async (api) => {
+                const nami = await import('nami-wallet-api')
+                // const cardanoSerialization = await import('@emurgo/cardano-serialization-lib-browser')
+                // setCardanoWASM(cardanoSerialization)
+
+                const namiAPI = await nami.NamiWalletApi(
+                    api,
+                    'testnetICmp5nDzUVyCh7DErS7ZoYg6Dn8Dc1TK'
+                )
+                setWalletAPI(namiAPI)
+
             }).catch((error) => { 
                 console.error(error) 
                 if (error.code === -3) {
@@ -91,18 +102,89 @@ export default function DBranchApp() {
         if(walletAPI !== null) {
             walletAPI.getNetworkId()
             .then((network) => {
-                switch(network) {
+                switch(network.id) {
                     case 0: setWalletNetwork('cardano testnet'); break
                     case 1: setWalletNetwork('cardano mainnet'); break
-                    default: setWalletNetwork('unknown cardano network: ' + network); break
+                    default: setWalletNetwork('unknown cardano network: ' + network.id + ') ' + network.network); break
                 }
             }).catch((error) => {
                 console.error(error)
                 setWalletError('Error getting wallet network')
                 setTimeout(() => { setWalletError(null) }, 5000)
             })
+
+            walletAPI.getAddress()
+                .then((address) => {
+                    setWalletAddress(address)
+                    console.log('address: ', address)
+                }).catch(error => {
+                    console.error(error)
+                    setWalletError('Error getting wallet address')
+                    setTimeout(() => { setWalletError(null) }, 5000)
+                })
+            
+            walletAPI.getUtxos()
+                .then((utxos) => {
+                    console.log('utxos: ', utxos)
+                }).catch(error => {
+                    console.error(error)
+                    setWalletError('Error getting utxos')
+                    setTimeout(() => { setWalletError(null) }, 5000)
+                })
+            
+            walletAPI.getUtxosHex()
+                .then((utxosHex) => {
+                    console.log('utxos hex: ', utxosHex)
+                }).catch(error => {
+                    console.error(error)
+                    setWalletError('Error getting utxos hex')
+                    setTimeout(() => { setWalletError(null) }, 5000)
+                })
+            
+            walletAPI.getAssets()
+                .then((assets) => {
+                    console.log('assets: ', assets)
+                }).catch(error => {
+                    console.error(error)
+                    setWalletError('Error getting assets')
+                    setTimeout(() => { setWalletError(null) }, 5000)
+                })
+                window.cardano.getBalance()
+                .then((balance) => {
+                    console.log('balance: ', balance)
+                }).catch(error => {
+                    console.error(error)
+                    setWalletError('Error getting balance')
+                    setTimeout(() => { setWalletError(null) }, 5000)
+                })
+            console.log('walletAPI: ', walletAPI)
+            console.log('cardano', window.cardano)
         }
     }, [walletAPI])
+
+    const sendTransaction = async () => {
+        console.log('sending transaction')
+        try{
+            let txId = await walletAPI.send({
+                address: walletAddress,
+                amount: .5,
+                metadata: {
+                    article: 'dbranch_intro.news',
+                    loc: 'ipfs://QmeJ3aARqJrAKjnyAEc88TQS32GdxXmA8JmFauKKiHPiYi'
+                },
+                metadataLabel: '451'
+            })
+
+            console.log('txId: ', txId)
+
+        }catch(error) {
+            console.error(error)
+            setWalletError(error.info)
+            setTimeout(() => { setWalletError(null) }, 5000)
+        }
+        
+        
+    }
 
 
 return (
@@ -118,8 +200,11 @@ return (
 
         <Button variant='primary' onClick={connectNami} disabled={walletAPI !== null}>Connect Nami</Button>
         &emsp;
-        {walletError && <span className='text-danger'>{walletError}</span>}
         {walletNetwork && <span>connected to: {walletNetwork}</span>}
+        {walletError && <span className='text-danger'><br />{walletError}</span>}
+
+        <br />
+        <Button variant='primary' onClick={sendTransaction} disabled={walletAddress === null}>Send Tx</Button>
             
         <hr />
 
